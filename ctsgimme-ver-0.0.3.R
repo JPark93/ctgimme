83f84225c2f1c57bd86f6bbdab88c1a0a9d11a77
Jonathan Park <imjpark@ucdavis.edu>
2025-08-07 13:23:28 -0700

    Update ctsgimme-ver-0.0.3.R

--- ctsgimme-ver-0.0.3.R
+++ ctsgimme-ver-0.0.3.R
@@ -205,6 +205,7 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
   parLapply(cl, ids, function(i) {
     DRIFT = diag(paste0("A_", 1:nvar, ",", 1:nvar), nvar)
     subset_dat = subset(dataframe, id == i)
+    # subset_dat[,varnames] = scale(subset_dat[,varnames])
     amat = mxMatrix("Full", nvar, nvar,
                      free   = DRIFT != "0",
                      name   = "A", 
@@ -255,12 +256,37 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
   }
   DRIFT = diag(paste0("A_", 1:nvar, 1:nvar), nvar)
   while(iterate < 1){
+    # rdss = list.files(paste0(directory, "/MIs/"), pattern = "\\.RDS$", full.names = TRUE)
+    # files = NULL
+    # EPCs = NULL
+    # for (file in rdss) {
+    #   file_id = gsub("MI_|\\.RDS", "", basename(file))
+    #   files = cbind(files, tryCatch({
+    #     c(readRDS(file)$"MI.Full")
+    #   }, error = function(e) {
+    #     message("Failed to read ", file, ": ", e$message)
+    #     NULL
+    #   }))
+    #   message(length(c(readRDS(file)$"MI.Full")))
+    #   EPCs = cbind(EPCs, tryCatch({
+    #     c(readRDS(file)$"EPC")
+    #   }, error = function(e) {
+    #     message("Failed to read ", file, ": ", e$message)
+    #     NULL
+    #   }))
+    # }
+    # sig1 = rowSums(pchisq(files, 1, lower.tail = FALSE) <= ks[count,])/ncol(files)
+    # sig2 = rowMeans(abs(EPCs))
+    # sigs = matrix(cbind(sig1, sig2), nrow(files))
     param_names = character(0)
     for (j in 1:nvar) {
       for (i in 1:nvar) {
+        # if (i != j) {
           param_names = c(param_names, sprintf("OUMod.A[%d,%d]", i, j))
+        # }
       }
     }
+    # param_names = sort(param_names)
     rdss = list.files(paste0(directory, "/MIs/"), pattern = "\\.RDS$", full.names = TRUE)
     files = NULL
     EPCs = NULL
@@ -289,6 +315,10 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
       prune = 0
       message("IT'S PRUNING TIME!")
       while(prune < 1){
+        if(sum(DRIFT != "0") == nvar){
+          prune = 1
+          break
+        }
         models = list()
         rdss1 = list.files(paste0(directory, "/Models/"), pattern = "\\.RDS$", full.names = TRUE)
         for(prn in 1:length(rdss1)){
@@ -436,6 +466,7 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
     })
     stopCluster(cl)
   }
+  
   ###---###---###---###---###---###---###
   # Subgrouping Stage
   ###---###---###---###---###---###---###
@@ -499,6 +530,7 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
   ###---###---###---###---###---###---###
   for(subgroup in sort(unique(memb))){
     DRIFT = G.DRIFT
+    saveRDS(DRIFT, paste0(directory, "/GStruc.RDS"))
     dir.create(paste0(directory, "/Models/Subgroup ", subgroup, "/"), showWarnings = FALSE)
     iterate = 0
     count = 1
@@ -674,9 +706,14 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
           for(i in 1:nrow(sum.fit$parameters)){
             effects[sum.fit$parameters$col[i],sum.fit$parameters$row[i]] = sum.fit$parameters$Estimate[i]
           }
+          # Determine significance stars
           sig = ifelse(abs(sum.fit$parameters$Estimate) > qnorm(0.975) * sum.fit$parameters$Std.Error, "*", "ns")
+          
+          # Format edge labels
           vals = cbind(round(sum.fit$parameters$Estimate, 2), sig)
           edge_labs = paste0(vals[, 2], " (", vals[, 1], ")")
+          
+          # Logical masks
           shared = G.DRIFT != "0" & DRIFT != "0"
           group_only = G.DRIFT == "0" & DRIFT != "0"
           colors = character(length = length(DRIFT))
@@ -686,7 +723,11 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
           colors[group_only & effects == 0] = "black"
           colors[DRIFT == "0"] = "black"
           colors = c(colors)
+          
+          # Output path
           output_path = file.path(directory, paste0("Models/Subgroup ", subgroup, "/Subgroup ", subgroup, " Params.png"))
+          
+          # Plot
           png(filename = output_path, width = 800, height = 800)
           qgraph(effects, layout = "circle", labels = varnames, 
                  edge.width = 1, diag = TRUE, edge.labels = edge_labs,
@@ -697,6 +738,7 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
             delt = (round(expm(effects * ints), 3))
             output_path = file.path(paste0(directory, "/Models/Subgroup ", subgroup, "/Subgroup ", subgroup, " Delta_t = ", ints,".png"))
             png(filename = output_path, width = 800, height = 800)
+            # Plot the subgroup structure
             qgraph(delt, layout = "circle", labels = varnames, fade = TRUE,
                    edge.width = 1, diag = TRUE, edge.labels = delt, maximum = 1.00,
                    theme = "colorblind", title = paste0("Subgroup ", subgroup, "; Delta_t = ", ints))
@@ -717,8 +759,10 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
         }
         nks = matrix(sort(nks, TRUE), m, 1)
         SG.DRIFT = DRIFT
+        saveRDS(SG.DRIFT, paste0(directory, "/SGStruc", subgroup,".RDS"))
         output_path = file.path(paste0(directory, "/Models/Subgroup ", subgroup, "/Subgroup ", subgroup, " Paths.png"))
         png(filename = output_path, width = 800, height = 800)
+        # Plot the subgroup structure
         qgraph(t(abs(((G.DRIFT != "0") * 1) - ((DRIFT != "0") * 1))), layout = "circle", labels = varnames, 
                edge.width = 5, diag = TRUE, edge.labels = paste0("SG-", subgroup))
         dev.off()
@@ -770,6 +814,25 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
               }
             }
             MIs = JPmx(fit, matrices = "A")
+          #   if(is.null(MIs) | is.null(MIs$MI.Full)){optimization = 1; fit = fit2}
+          #   if(abs(MIs$MI.Full)[which.max(abs(MIs$MI.Full))] >= qchisq(1-nks[count,], df = 1)){
+          #     cells = as.numeric(unlist(regmatches(names(which.max(MIs$MI.Full)),
+          #                                          gregexpr("\\d+", names(which.max(MIs$MI.Full))))))
+          #     
+          #     osc$A$free[cells[1], cells[2]] = TRUE
+          #     osc$A$labels[cells[1], cells[2]] = paste0("A_", cells[1], ",", cells[2])
+          #     message(paste0("Adding drift parameter A[", cells[1], ",", cells[2],"]"))
+          #     MIs = NULL
+          #     count = count + 1
+          #     if(sum(osc$A$free) == nvar^2){
+          #       optimization = 1
+          #     }
+          #   }else{
+          #     optimization = 1
+          #   }
+          # }
+            
+            
             if (is.null(MIs) || is.null(MIs$MI.Full)) {
               optimization = 1
               fit = fit2
@@ -872,6 +935,8 @@ ctsgimme = function(varnames = NULL, dataframe = NULL,
         packs = list('ctsem', 'ctsemOMX', 'dynr', 'OpenMx', 'qgraph')
         invisible(lapply(packs, require, character.only = T))
       })
+      # qmat = mxMatrix('Diag', nvar, nvar, FALSE, PE.var, name='Q')
+      # rmat = mxMatrix('Diag', nvar, nvar, FALSE, ME.var, name='R')
       parLapply(cl, valid_ids, function(i) {
         subset_dat = subset(new.data, id == i)
         amat = mxMatrix("Full", nvar, nvar,
